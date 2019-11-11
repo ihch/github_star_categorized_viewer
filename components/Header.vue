58fe3c7e70d0bea0087fbe0f10a905ab75b002c4
nemusou <ryubi.102395@gmail.com>
2019-11-11 23:20:10 +0900

    feat: Header componentの実装

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <div class="header__title">Header Title</div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+})
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  color: #cfd0d1;
+  background-color: #24292e;
+  height: 65px;
+  padding: 16px;
+}
+
+.header__title {
+  color: rgba(255, 255, 255, 0.9);
+  font-size: 2rem;
+  text-align: justify;
+}
+</style>
