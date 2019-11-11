9542d3283ff4740404f17ca63432ea1f523988e9
nemusou <ryubi.102395@gmail.com>
2019-11-11 23:20:27 +0900

    feat: index pageにHeaderをつけた

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container">
+    <Header />
     <Inputform />
   </div>
 </template>
 
 <script>
 import Inputform from '~/components/Inputform.vue';
+import Header from '~/components/Header.vue';
 
 export default {
   components: {
-    Inputform
+    Inputform,
+    Header
   }
 }
 </script>
