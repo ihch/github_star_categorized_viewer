51a7ac9ab689c9b5204bdf0531f744249c19efe4
nemusou <ryubi.102395@gmail.com>
2019-11-11 22:36:30 +0900

    feat: 雑なinputform componentを作った

---
+++ components/Inputform.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="text" v-model="username">
+    <p>your name {{ username }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: ""
+    };
+  }
+}
+</script>
+
+<style scoped>
+</style>
